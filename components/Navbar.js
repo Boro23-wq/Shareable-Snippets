562462de1d95bf6a27b95962cf0d776a7bef5247
Boro23 <404slimboysavage@gmail.com>
2021-03-01 19:27:58 +0530

    modified

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,45 +10,24 @@ import {
 } from '../styles/icon';
 
 export default function Navbar() {
-  const { user, isLoading } = useUser();
+  const { user } = useUser();
 
   return (
     <nav className='flex flex-col items-center justify-center'>
-      {/* <Link href='/' passHref> */}
-      {/* <a className='text-3xl mb-2 block text-center text-gray-800 font-black uppercase'>
-          Shareable Snippets
-        </a> */}
       <div className='m-l-20 mb-2 flex justify-center cursor-pointer'>
         <Link href='/'>
-          {/* <FastFeedbackIcon color='black' boxSize='62px' mb={2} /> */}
           <ShareableSnippetsIcon color='black' boxSize='62px' mb={4} />
         </Link>
       </div>
-      {/* </Link> */}
       <div className='space-x-3 m-x-auto mb-6 justify-center'>
-        {/* <Link href='/snippets/html'>
-          <a className='text-red-100 font-bold px-2 border-2 border-red-400 rounded-md hover:border-red-200 transition duration-500 ease-in-out'>
-            HTML
-          </a>
-        </Link> */}
-        {/*<Link href='/snippets/css'>
-          <a className='text-red-100 font-bold px-2 border-2 border-red-400 rounded-md hover:border-red-200 transition duration-500 ease-in-out'>
-            CSS
-          </a>
-        </Link>
-        <Link href='/snippets/javascript'>
-          <a className='text-red-100 font-bold px-2 border-2 border-red-400 rounded-md hover:border-red-200 transition duration-500 ease-in-out'>
-            JavaScript
-          </a>
-        </Link> */}
-        {!isLoading && !user && (
+        {!user && (
           <Link href='/api/auth/login'>
             <a className='text-md text-gray-100 border-2 border-gray-700 bg-gray-900 font-bold px-4 py-2 rounded-md hover:bg-gray-800 transition duration-500 ease-in-out'>
               Login <LoginIcon className='ml-2' />
             </a>
           </Link>
         )}
-        {!isLoading && user && (
+        {user && (
           <div className='sm:flex'>
             <div className='sm:flex-row '>
               <Link href='/mySnippets'>
