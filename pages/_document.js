0650549c84622da89d39282e6ec7aa627a88ff5f
Boro23 <404slimboysavage@gmail.com>
2021-03-01 17:51:16 +0530

    modified

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html className='bg-blue-100' lang='en'>
         <Head>
           <link
             rel='preload'
